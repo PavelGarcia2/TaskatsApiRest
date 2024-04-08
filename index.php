efa7c7c17ec6dff0890ba6b2017692ae3b999203
PavelGarcia2 <60678482+PavelGarcia2@users.noreply.github.com>
2024-04-08 16:02:31 +0200

    POST method for creatin a user not handling encryption yet

--- index.php
+++ index.php
@@ -19,32 +19,24 @@ Flight::route('GET /', function () {
     ];
 
     echo json_encode($array);
-
 });
 
-Flight::route('GET '.$home.'users/', function () {
+Flight::route('GET ' . $home . 'users/', function () {
     $db = Flight::db();
     $query = $db->prepare("SELECT * FROM user");
     $query->execute();
     $data = $query->fetchAll(PDO::FETCH_ASSOC);
-    
+
     $array = [];
-    foreach ($data as $row){
-        $array[]=[
+    foreach ($data as $row) {
+        $array[] = [
             "id" => $row['user_id'],
             "name" => $row['username'],
             "email" => $row['email']
-        ] ;           
+        ];
     }
-    
-    /*
-    $array = [
-        "texto" => "Hello World",
-        "status" => "success"
-    ];
 
-    echo json_encode($array);
-    */
+
     Flight::json([
         "total_rows" => $query->rowCount(),
         "rows" => $array
@@ -52,25 +44,81 @@ Flight::route('GET '.$home.'users/', function () {
 });
 
 
-Flight::route('GET '.$home.'dailytasks/', function () {
+Flight::route('GET ' . $home . 'users/@id', function ($id) {
+    $db = Flight::db();
+    $query = $db->prepare("SELECT * FROM user WHERE user_id= :id");
+    $query->execute([":id" => $id]);
+    $data = $query->fetch(PDO::FETCH_ASSOC);
+
+    $array = [
+        "name" => $data['username'],
+        "id" => $data['user_id'],
+        "email" => $data['email']
+    ];
+
+
+
+    Flight::json($array);
+});
+
+
+Flight::route('POST ' . $home . 'users/', function () {
+    $db = Flight::db();
+
+    $name = Flight::request()->data->name;
+    $email = Flight::request()->data->email;
+    $password = Flight::request()->data->password;
+
+    try {
+        $query = $db->prepare("INSERT INTO user (username, password, email) VALUES (:username, :pass, :email)");
+
+        if ($query->execute([":username" => $name, ":pass" => $password, ":email" => $email])) {
+            $array = [
+                "data" => [
+                    "id" => $db->lastInsertId(),
+                    "name" => $name,
+                    "password" => $password,
+                    "email" => $email
+                ],
+                "status" => "success"
+            ];
+        } else {
+            $array = [
+                "error" => "Hubo un error al insertar los registros",
+                "status" => "error"
+            ];
+        }
+    } catch (PDOException $e) {
+        // Captura la excepción de la base de datos y maneja el error
+        $array = [
+            "error" => "Hubo un error en la base de datos: " . $e->getMessage(),
+            "status" => "error"
+        ];
+    }
+
+    Flight::json($array);
+});
+
+
+Flight::route('GET ' . $home . 'dailytasks/', function () {
     $db = Flight::db();
     $query = $db->prepare("SELECT * FROM dailytask JOIN task ON dailytask.task_id = task.task_id JOIN project ON task.project_id=project.project_id");
     $query->execute();
     $data = $query->fetchAll(PDO::FETCH_ASSOC);
-    
+
 
     $tasks = [];
-    foreach ($data as $row){
-        $tasks[]=[
-            "ID"=> $row['daily_task_id'],
-            "Name"=> $row['title'], // to implement with join
-            "Task_id"=> $row['task_id'],
-            "Date"=> $row['date'],
-            "Status"=> $row['completed'],
-            "Color"=> $row['color'],
-            "Punctuation"=> "3" //to implement in database and with join
+    foreach ($data as $row) {
+        $tasks[] = [
+            "ID" => $row['daily_task_id'],
+            "Name" => $row['title'],
+            "Task_id" => $row['task_id'],
+            "Date" => $row['date'],
+            "Status" => $row['completed'],
+            "Color" => $row['color'],
+            "Punctuation" => "3" //to implement in database and with join
 
-        ] ;           
+        ];
     }
     /*
     $array = [
@@ -84,12 +132,12 @@ Flight::route('GET '.$home.'dailytasks/', function () {
 });
 
 
-Flight::route('GET '.$home.'projects/', function () {
+Flight::route('GET ' . $home . 'projects/', function () {
     $db = Flight::db();
     $query = $db->prepare("SELECT * FROM project ");
     $query->execute();
     $data = $query->fetchAll(PDO::FETCH_ASSOC);
-    
+
 
     /*
     $array = [
@@ -99,7 +147,7 @@ Flight::route('GET '.$home.'projects/', function () {
 
     
     */
-    
+
     Flight::json(["projects" => $data]);
 });
 
